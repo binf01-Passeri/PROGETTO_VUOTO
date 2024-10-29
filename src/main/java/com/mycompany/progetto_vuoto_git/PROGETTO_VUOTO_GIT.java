4ad3ca8c203de3756e1d5ed08d069157b5de2b64
binf01_Passeri <riccardopasseri06@gmail.com>
2024-10-29 13:13:32 +0100

    primo caricamento

---
+++ src/main/java/com/mycompany/progetto_vuoto_git/PROGETTO_VUOTO_GIT.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.progetto_vuoto_git;
+
+/**
+ *
+ * @author B045_07
+ */
+public class PROGETTO_VUOTO_GIT {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
